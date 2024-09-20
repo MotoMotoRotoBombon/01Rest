6aafd57733aae2d4b97dd7586276830f92de48cc
Rofriasbo <rofriasbo@ittepic.edu.mx>
2024-09-20 09:20:22 -0700

    01.test
    primer test

--- test/tasks.test.js
+++ test/tasks.test.js
@@ -3,33 +3,27 @@ const chai = require('chai');
 const sinon = require('sinon');
 
 const app = require('../index');
-
 const taskController = require('../controllers/taskController');
 const expect = chai.expect;
 
 describe('GET /tasks', () => {
-    it('Deberia de devolver las tareas con status 200 cuando hay tareas'), async () => {
+    it('Debería devolver las tareas con status 200 cuando hay tareas', async () => {
         const tasks = [
             {
                 id: 1,
                 title: "Tarea1",
-                description: "Descripción de la tarea 1"
+                description: "Descripción de la tarea 1",
+                startDate: '20-09-24',
+                endDate: '20-09-24',
+                status: 'pendiente',
+                teamMembers: ["Reymundo Sahagún", "Rodolfo Frias", "Jorge Gonzalez", "Cesar Uriel"],
             },
-            {
-                id: 2,
-                title: "Tarea2",
-                description: "Descripción de la tarea 2"
-            },
-            {
-                id: 3,
-                title: "Tarea3",
-                description: "Descripción de la tarea 3"
-            }
         ];
+
         const res = await request(app).get('/tasks');
         expect(res.status).to.equal(200);
         expect(res.body).to.be.an('array');
-        expecto(res.body.lenght).to.equal(3);
+        expect(res.body.length).to.equal(1);
         expect(res.body).to.deep.equal(tasks);
-    };
+    });
 });
