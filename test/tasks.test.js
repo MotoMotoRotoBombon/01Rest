67ec8ce7b8da356019321f6a47f19b1459251f1b
Cesar Hernandez <149346749+cesarurielhr@users.noreply.github.com>
2024-09-24 00:09:17 -0700

    test

--- test/tasks.test.js
+++ test/tasks.test.js
@@ -3,33 +3,54 @@ const chai = require('chai');
 const sinon = require('sinon');
 
 const app = require('../index');
-
 const taskController = require('../controllers/taskController');
 const expect = chai.expect;
 
 describe('GET /tasks', () => {
-    it('Deberia de devolver las tareas con status 200 cuando hay tareas'), async () => {
-        const tasks = [
-            {
-                id: 1,
-                title: "Tarea1",
-                description: "Descripción de la tarea 1"
-            },
-            {
-                id: 2,
-                title: "Tarea2",
-                description: "Descripción de la tarea 2"
-            },
-            {
-                id: 3,
-                title: "Tarea3",
-                description: "Descripción de la tarea 3"
-            }
-        ];
+    it('Debería devolver las tareas con status 200 cuando hay tareas', async () => {
+        const tasks = [ 
+    {
+        id: 1,
+        title: "Tarea1",
+        description: "Descripción de la tarea 1",
+        startDate: '2024-09-20',
+        endDate: '2024-11-28',
+        status: 'completado',
+        teamMembers : ["Reymundo Sahagún","Rodolfo Frias","Jorge Gonzalez","Cesar Uriel"],
+        budget: 5000,
+    },
+  
+];
+
+
         const res = await request(app).get('/tasks');
         expect(res.status).to.equal(200);
         expect(res.body).to.be.an('array');
-        expecto(res.body.lenght).to.equal(3);
+        expect(res.body.length).to.equal(1);
         expect(res.body).to.deep.equal(tasks);
+    });
+});
+
+describe('GET /tasks/1', () => {
+
+    it('2. Debería devolver un consulta por id con status 200', async() => {
+       it('Debería devolver las tareas con status 200 cuando hay tareas', async () => {
+        const tasks = {
+        id: 1,
+        title: "Tarea1",
+        description: "Descripción de la tarea 1",
+        startDate: '2024-09-20',
+        endDate: '2024-11-28',
+        status: 'completado',
+        teamMembers : ["Reymundo Sahagún","Rodolfo Frias","Jorge Gonzalez","Cesar Uriel"],
+        budget: 5000,
     };
+
+        const res = await request(app).get('/tasks/1');
+
+        expect(res.status).to.equal(200);
+        expect(res.body).to.be.an('object');
+        expect(res.body).to.deep.equal(tasks);
+    });
+});
 });
